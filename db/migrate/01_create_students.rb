59502dbf8b7eb6d7af3f553f1ecf92f63c47bff1
Cory <oneillco117@gmail.com>
2021-11-13 11:14:39 -0500

    commit tree a60984466030b73f2b0dd30732aa10aab078fb6b

--- db/migrate/01_create_students.rb
+++ db/migrate/01_create_students.rb
@@ -1,4 +1,10 @@
 class CreateStudents < ActiveRecord::Migration[6.1]
   def change
+    create_table :products do |t|
+      t.string :name
+      t.text :description
+
+      t.timestamps
+    end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -12,4 +12,11 @@
 
 ActiveRecord::Schema.define(version: 1) do
 
+  create_table "products", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
 end
